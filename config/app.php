c2a93a40ed987b108a98f8931e2959d56b176340
ttanaka9211 <ttanaka@hatchdogs.net>
2020-08-02 09:29:33 +0900

    localに変更

--- config/app.php
+++ config/app.php
@@ -274,9 +274,9 @@ return [
             /*
              * It is recommended to set these options through your environment or app_local.php
              */
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'database' => env('DB_DATABASE'),
+            //'username' => env('DB_USERNAME'),
+            //'password' => env('DB_PASSWORD'),
+            //'database' => env('DB_DATABASE'),
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
