990bd7e1499f675809b874649366f6b83d065144
ttanaka9211 <ttanaka@hatchhdogs.net>
2020-08-07 16:26:51 +0900

    phpunit test

---
+++ tests/TestCase/View/Helper/ProgressHelperTest.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Test\TestCase\View\Helper;
+
+use App\View\Helper\ProgressHelper;
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+
+/**
+ * App\View\Helper\ProgressHelper Test Case
+ */
+class ProgressHelperTest extends TestCase
+{
+    /**
+     * Test subject
+     *
+     * @var \App\View\Helper\ProgressHelper
+     */
+    public $Progress;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $view = new View();
+        $this->Progress = new ProgressHelper($view);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Progress);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test bar method
+     *
+     * @return void
+     */
+    public function testBar()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
